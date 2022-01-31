fc8fea3e0bb88a25a8bb922065253f9c4ea12838
Lucas Martins <lucasmartins.ufrgs@gmail.com>
2022-01-31 17:27:23 -0300

    Adicionando PreparedStatemend para evitar SQLINJECTION

--- src/TestaConexao.java
+++ src/TestaConexao.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,11 +13,21 @@ public class TestaConexao {
 			DriverManager.getConnection
 			("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "8h7mbebesd");
 	
-			Statement stm = connection.createStatement();
-			stm.execute("DELETE FROM PRODUTO WHERE ID > 2");
+			String nome = "mouse 2'";
+			String descricao = "mouse sem fio 2";
+	
+			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", 
+					Statement.RETURN_GENERATED_KEYS);
+			stm.setString(1, nome);
+			stm.setString(2,descricao);
 			
-			Integer linhasModificadas = stm.getUpdateCount();
-			System.out.println("Linhas excluidas: " + linhasModificadas);
+			stm.execute();
+			
+			ResultSet rst = stm.getGeneratedKeys();
+			while(rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O id criado foi: "+id);
+			}
 			
 	connection.close();
 	}
