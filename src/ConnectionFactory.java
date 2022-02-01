aa0609edc6ca6e5899e18b1cbc7d4c9e9cfca7e0
Lucas Martins <lucasmartins.ufrgs@gmail.com>
2022-02-01 16:54:00 -0300

    Adicionando o Connection Factory com um Pool de Conexões

---
+++ src/ConnectionFactory.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory{
+	
+	public DataSource dataSource;
+	
+	public ConnectionFactory() {
+		
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("8h7mbebesd");
+		this.dataSource = comboPooledDataSource;
+	}
+	
+	public Connection recuperarConexao() throws SQLException{
+		return this.dataSource.getConnection();
+	}
+}
