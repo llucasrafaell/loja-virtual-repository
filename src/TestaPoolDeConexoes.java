8defca437e26c2e0654dd0e5d8ee6182ea3d1a89
Lucas Martins <lucasmartins.ufrgs@gmail.com>
2022-02-01 18:07:03 -0300

    Adicionando um modelo de Produto

--- src/TestaPoolDeConexoes.java
+++ src/TestaPoolDeConexoes.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
 
 public class TestaPoolDeConexoes {
 	
@@ -10,9 +12,20 @@ public class TestaPoolDeConexoes {
 		Connection connection = connectionFactory.recuperarConexao();
 		connection.setAutoCommit(false);
 		
-		for (int i = 0; i < 20; i++) {
-			connectionFactory.recuperarConexao();
-			System.out.println("Conexao de numero:" + i);
-		} 
+		Produto comoda = new Produto("Comoda","Comoda Vertical");
+		
+		String sql = "INSERT INTO PRODUTO (NOME,DESCRICAO) VALUES (?,?)";
+		try(PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+			pstm.setString(1, comoda.getNome());
+			pstm.setString(2, comoda.getDescricao());
+			pstm.execute();
+			
+			try(ResultSet rst = pstm.getGeneratedKeys()){
+				while (rst.next()) {
+					comoda.setId(rst.getInt(1));
+				}
+			}
+		}
+		System.out.println(comoda);
 	}
 }
